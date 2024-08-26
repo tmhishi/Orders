98af872c7a7a4570f7e2383670ab60752b926fe2
Tinashe Mhishi <tmhishi777@gmail.com>
2024-08-26 09:42:59 +0200

    Add project files.

---
+++ Models/GeniiAnalyticsContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Orders.Models;
+
+public partial class GeniiAnalyticsContext : DbContext
+{
+    public GeniiAnalyticsContext()
+    {
+    }
+
+    public GeniiAnalyticsContext(DbContextOptions<GeniiAnalyticsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrdersId).HasName("PK__orders__B46F683302023816");
+
+            entity.ToTable("orders");
+
+            entity.Property(e => e.OrdersId)
+                .ValueGeneratedNever()
+                .HasColumnName("orders_id");
+            entity.Property(e => e.OrderDate).HasColumnName("order_date");
+            entity.Property(e => e.OrderStatus).HasColumnName("order_status");
+            entity.Property(e => e.StaffId).HasColumnName("staff_id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orders.Models;
+
+public partial class Order
+{
+    public int OrdersId { get; set; }
+
+    public DateOnly? OrderDate { get; set; }
+
+    public int? OrderStatus { get; set; }
+
+    public int? StaffId { get; set; }
+}
